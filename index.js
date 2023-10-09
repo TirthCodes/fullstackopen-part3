b84e167614e89607b339be0245e11c0b59af2af6
Tirth Patel <tirthpatelcse@gmail.com>
2023-10-09 13:38:07 +0530

    Exercise 3.7 Added morgan logging middleware with tiny configuration

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(morgan('tiny'))
 
 const PORT = 3001;
 
